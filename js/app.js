bba625e41c895e57da9addfe7d3a51cd4767d85b
Ailsa Young <ailsa.young16@hotmail.co.uk>
2020-12-13 19:08:33 +0000

    added delete button and handle event function

--- js/app.js
+++ js/app.js
@@ -2,13 +2,17 @@ document.addEventListener("DOMContentLoaded",() => {
 
     const newItemForm = document.querySelector("#new-character-form");
     newItemForm.addEventListener("submit", handleFormSubmit);
+    
+    const characterList= document.querySelector("#character-list");
+    
+    createDeleteButton();
 
-    const deleteAllButton = document.createElement("button");
-
+    const deleteAllButton = document.querySelector("#delete-button")
+    deleteAllButton.addEventListener('click', handleDeleteButton);
+     
 })
 
-// NOTE TO SELF: MAKE THIS D&D CHARACTER TRACKER INSTEAD!
-
+// Function that reacts to form submission and populates list with entries
 const handleFormSubmit = function (event) {
     // Prevents refresh
     event.preventDefault();
@@ -26,20 +30,44 @@ const createCharacterListItem = function (form){
     characterListItem.classList.add("character-list-item");
     // Creates character name variable and appends to created list item
     const characterName = document.createElement("h2");
-    characterName.textContent = form.title.value;
+    characterName.textContent = `Name: ${form.name.value}`;
     characterListItem.appendChild(characterName);
     // Creates character race variable and appends to created list item
     const characterRace = document.createElement("h3");
-    characterRace.textContent = form.race.value;
+    characterRace.textContent = `Race: ${form.race.value}`;
     characterListItem.appendChild(characterRace);
     // Creates character class variable and appends to created list item (includes subclass in listing)
     const characterClass = document.createElement("h3");
-    characterClass.textContent = `${form.class.value} (${form.subclass.value})`;
+    characterClass.textContent = `Class: ${form.class.value} (${form.subclass.value})`;
     characterListItem.appendChild(characterClass);
     // Creates character level variable and appends to created list item
     const characterLevel = document.createElement ("h3");
-    characterLevel.textContent = form.level.value;
+    characterLevel.textContent = `Level: ${form.level.value}`;
     characterListItem.appendChild(characterLevel);
     
     return characterListItem;
+}
+
+
+// Function to clear character list when delete button is clicked
+const handleDeleteButton = function (){
+    const characterList = document.querySelector('#character-list');
+    characterList.innerHTML = '';
+}
+
+// Function that creates delete all button
+const createDeleteButton = function (){
+    const parentElement = document.querySelector("h1");
+
+    const deleteAllButton = document.createElement("BUTTON");
+    deleteAllButton.textContent = "Clear Character List"
+    deleteAllButton.id = "delete-button"
+    deleteAllButton.classList.add('btn')
+
+   parentElement.appendChild(deleteAllButton);
+}
+
+// (CAN'T GET THIS TO WORK) Helper function checking if there is any content in the list of characters
+const listNotEmpty = function (element){
+    return !!element.childElementCount;
 }
